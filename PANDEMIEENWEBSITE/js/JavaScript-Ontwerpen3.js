5945dadd5413940d6453d69e0b06d6cfba01a68e
Nasséra <45563624+DashingNass@users.noreply.github.com>
2020-06-15 06:41:12 +0200

    Add files via upload

---
+++ PANDEMIEENWEBSITE/js/JavaScript-Ontwerpen3.js
@@ -0,0 +1,15 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console': 0*/
+
+
+/*code afkomstig van:
+https://www.w3schools.com/howto/howto_js_scroll_indicator.asp*/
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("scrollie").style.width = scrolled + "%";
+}
